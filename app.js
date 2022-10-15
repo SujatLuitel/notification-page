549843c6e7ce5071755873e1ea026225373c02c6
SujatLuitel <107681024+SujatLuitel@users.noreply.github.com>
2022-10-15 15:29:29 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+const unReadMessages = document.querySelectorAll('.unread');
+const unRead = document.getElementById('number');
+const markAll = document.getElementById('mark_all')
+
+unRead.innerText = unReadMessages.length;
+
+unReadMessages.forEach((message) => {
+    message.addEventListener('click', () => {
+        message.classList.remove('unread');
+        const newUnReadMessages = document.querySelectorAll('.unread');
+        unRead.innerText = newUnReadMessages.length;
+    });
+});
+
+markAll.addEventListener('click', () => {
+    unReadMessages.forEach((message) => {
+        message.classList.remove('unread');
+    });
+    const newUnReadMessages = document.querySelectorAll('.unread');
+    unRead.innerText = newUnReadMessages.length;
+});
